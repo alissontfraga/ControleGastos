eb5beabdb84d1e6844ef40c716e85336498da458
alissontfraga <alissontfraga@gmail.com>
2026-07-11 17:47:29 -0300

    refactor: reorganizar a classe TransacaoRequest para incluir validações de dados

--- Backend/ControleGastos.API/DTOs/Transacoes/TransacaoRequest.cs
+++ Backend/ControleGastos.API/DTOs/Transacoes/TransacaoRequest.cs
@@ -1,10 +1,21 @@
-namespace ControleGastos.API.DTOs.Transacoes;
-
+using System.ComponentModel.DataAnnotations;
 using ControleGastos.API.Enums;
 
-public record TransacaoRequest(
-    string Descricao,
-    decimal Valor,
-    TipoTransacao Tipo,
-    Guid PessoaId
-);
+namespace ControleGastos.API.DTOs.Transacoes
+{
+    public record TransacaoRequest
+    (
+        [Required(ErrorMessage = "A descrição é obrigatória.")]
+        [MaxLength(250, ErrorMessage = "A descrição deve ter no máximo 250 caracteres.")]
+        string Descricao,
+
+        [Range(typeof(decimal), "0,01", "999999999", ErrorMessage = "O valor deve ser maior que zero.")]
+        decimal Valor,
+
+        [Required(ErrorMessage = "O tipo da transação é obrigatório.")]
+        TipoTransacao Tipo,
+
+        [Required(ErrorMessage = "A pessoa é obrigatória.")]
+        Guid PessoaId
+    );
+}
