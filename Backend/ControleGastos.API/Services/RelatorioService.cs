6fc33180df524cbac508c0a5e3ad15e9e2ab168b
alissontfraga <alissontfraga@gmail.com>
2026-07-11 17:50:31 -0300

    feat: implementar a classe RelatorioService para cálculo do resumo dos gastos

---
+++ Backend/ControleGastos.API/Services/RelatorioService.cs
@@ -0,0 +1,36 @@
+using ControleGastos.API.Data;
+using ControleGastos.API.DTOs.Relatorios;
+using ControleGastos.API.Enums;
+using ControleGastos.API.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleGastos.API.Services
+{
+    public class RelatorioService(AppDbContext context) : IRelatorioService
+    {
+        private readonly AppDbContext _context = context;
+
+        // Método responsável por calcular o resumo dos gastos
+        public async Task<ResumoGastosResponse> ObterResumoAsync()
+        {
+            var transacoes = await _context.Transacoes
+                .ToListAsync();
+
+            var totalReceitas = transacoes
+                .Where(t => t.Tipo == TipoTransacao.Receita)
+                .Sum(t => t.Valor);
+
+            var totalDespesas = transacoes
+                .Where(t => t.Tipo == TipoTransacao.Despesa)
+                .Sum(t => t.Valor);
+
+            var saldo = totalReceitas - totalDespesas;
+
+            return new ResumoGastosResponse(
+                totalReceitas,
+                totalDespesas,
+                saldo
+            );
+        }
+    }
+}
