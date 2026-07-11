99eb628ee8d9763861fa53cda934d08441253b6c
alissontfraga <alissontfraga@gmail.com>
2026-07-11 14:38:34 -0300

    feat: configurar contexto do Entity Framework

---
+++ Backend/ControleGastos.API/Data/AppDbContext.cs
@@ -0,0 +1,36 @@
+using ControleGastos.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleGastos.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {}
+
+        public DbSet<Pessoa> Pessoas => Set<Pessoa>();
+
+        public DbSet<Transacao> Transacoes => Set<Transacao>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Entidade Pessoa
+            modelBuilder.Entity<Pessoa>()
+            .HasMany(p => p.Transacoes)
+            .WithOne(t => t.Pessoa)
+            .HasForeignKey(t => t.PessoaId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+            // Entidade Transação
+            modelBuilder.Entity<Transacao>()
+            .Property(t => t.Tipo)
+            .HasConversion<string>();
+
+            modelBuilder.Entity<Transacao>()
+            .Property(t => t.Valor)
+            .HasPrecision(18,2);
+        }
+    }
+}
