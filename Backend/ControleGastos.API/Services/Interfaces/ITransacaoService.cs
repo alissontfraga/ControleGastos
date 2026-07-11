9516f99156350454a51e4a68952688c12a2f8206
alissontfraga <alissontfraga@gmail.com>
2026-07-11 17:23:21 -0300

    feat: criar interface ITransacaoService para gerenciamento de transações

---
+++ Backend/ControleGastos.API/Services/Interfaces/ITransacaoService.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ControleGastos.API.DTOs.Transacoes;
+
+namespace ControleGastos.API.Services.Interfaces
+{
+    public interface ITransacaoService
+    {
+    // Método responsável por criar uma nova transação
+    Task<TransacaoResponse> CriarAsync(TransacaoRequest request);
+
+    // Método responsável por buscar todas as transações
+    Task<IEnumerable<TransacaoResponse>> BuscarTodasAsync();
+
+    // Método responsável por buscar uma transação por ID
+    Task<TransacaoResponse?> BuscarPorIdAsync(Guid id);
+
+    // Método responsável por remover uma transação por ID
+    Task<bool> RemoverAsync(Guid id);
+    }
+}
