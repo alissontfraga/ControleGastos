249243f6b3132c86c18f0c79c84ab5b08f43dd68
alissontfraga <alissontfraga@gmail.com>
2026-07-11 17:49:59 -0300

    refactor: reorganizar o DTO PessoaRequest para incluir validações de dados

--- Backend/ControleGastos.API/DTOs/Pessoas/PessoaRequest.cs
+++ Backend/ControleGastos.API/DTOs/Pessoas/PessoaRequest.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
 
 namespace ControleGastos.API.DTOs.Pessoas
 {
-    public record PessoaRequest(
+    public record PessoaRequest
+    (
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        [MaxLength(80, ErrorMessage = "O nome deve ter no máximo 80 caracteres.")]
         string Nome,
+
+        [Required(ErrorMessage = "A idade é obrigatória.")]
+        [Range(1, 110, ErrorMessage = "A idade deve estar entre 1 e 110 anos.")]
         int Idade
     );
-    
 }
