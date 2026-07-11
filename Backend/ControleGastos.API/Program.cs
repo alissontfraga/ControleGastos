7b7a9a6a11ff268226f7669101983ed1f73fd015
alissontfraga <alissontfraga@gmail.com>
2026-07-11 16:47:19 -0300

    feat: adicionar configuração do serviço IPessoaService no Program.cs

--- Backend/ControleGastos.API/Program.cs
+++ Backend/ControleGastos.API/Program.cs
@@ -1,4 +1,6 @@
 using ControleGastos.API.Data;
+using ControleGastos.API.Services;
+using ControleGastos.API.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +13,9 @@ builder.Services.AddOpenApi();
 
 /* Configuração do Banco de Dados */
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Configuração dos Serviços
+builder.Services.AddScoped<IPessoaService, PessoaService>();
 
 var app = builder.Build();
 
